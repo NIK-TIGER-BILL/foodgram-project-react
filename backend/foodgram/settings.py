c355a9b0ecbd450858f57d485af384dfac3e8952
Nikita Notebook <nik.tigr2010@gmail.com>
2021-10-08 00:19:07 +0300

    solve errors

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('DJ_SECRET_KEY'),
 
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['*']
 
